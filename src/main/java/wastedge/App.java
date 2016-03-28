d3d46241872b3d2231d2a46160111daeacc7053b
Pieter van Ginkel <pginkel@gmt.nl>
2016-03-28 18:04:36 +0200

    Initial commit.

---
+++ src/main/java/wastedge/App.java
@@ -0,0 +1,7 @@
+package wastedge;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/wastedge/AppTest.java
@@ -0,0 +1,11 @@
+package wastedge;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class AppTest {
+}
